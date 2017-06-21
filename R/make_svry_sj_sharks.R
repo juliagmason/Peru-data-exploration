d299a2708a67a7c1f38ec27054c908a4ef349ca3
Julia Mason <jgmason@stanford.edu>
2017-06-21 16:15:18 -0700

    Code to make csvs of Svry shark sets and trips, put in PD sharks project

--- R/make_svry_sj_sharks.R
+++ R/make_svry_sj_sharks.R
@@ -6,6 +6,14 @@ library (tidyverse)
 gn.trips <- read.csv ("./Data_tables/probable_gillnets.csv")
 gn.trips$Fecha.de.zarpe <- as.Date (gn.trips$Fecha.de.zarpe, format = "%Y-%m-%d")
 
+set.DLL <- read.csv("./Data_tables/set_DLL.csv")
+set.DLL$Lance.code <- as.character( (set.DLL$Lance.code))
+
+sharks <- read.csv("./Raw_data/sharks_main.csv") # messed with shark_main already
+sharks$Lance.code <- paste(sharks$trip.code, sharks$lance, sep = ".")
+sharks <- left_join(sharks, set.DLL, by = "Lance.code") %>%
+  rename (Species = species, Trip.code = trip.code) # fix capitals to match other data frames
+
 # all.gn <- filter (trips, !Net.depth.category %in% "Fondo", Sistema %in% c("cortina", "manual", "red", "rayera", "mechanico","mixed nets"), !is.na(trips$panes) | !is.na(trips$pane.1), linea.madre.net.depth != "profundidad", Lugar.de.zarpe != "constante") # use a couple data columns in case. if there's hooks, that's longlines
 
 svry.trip <- filter (gn.trips, Lugar.de.zarpe == "Salaverry", Fecha.de.zarpe >= "2005-01-01")
@@ -13,8 +21,8 @@ svry.trip.extra <- filter (gn.trips, Trip.code %in% c(264, 564, 1433))
 svry.trip <- rbind (svry.trip, svry.trip.extra) # 289 trips
 write.csv (svry.trip, file = "./Data_tables/svry_trip.csv", row.names = FALSE)
 
-
-svry.sharks <- filter (sharks, trip.code %in% svry.trip$Trip.code) # 8615 sharks
+# Make a csv of just the sharks in Salaverry, and save it into the Sharks project
+svry.sharks <- filter (sharks, Trip.code %in% svry.trip$Trip.code) # 8615 sharks
 write.csv (svry.sharks, file = "../PDsharks/Salaverry_sharks.csv", row.names = FALSE)
 
 sj.trip <- filter (gn.trips, Lugar.de.zarpe == "San Jose" | Lugar.de.zarpe == "Bayovar" | Lugar.de.zarpe == "bayovar")
@@ -22,6 +30,17 @@ sj.trip.extra <- filter (gn.trips, Trip.code == 599) # ? this doesn't exist anym
 sj.trip <- rbind (sj.trip, sj.trip.extra) # 94 trips
 
 
-sj.sharks <- filter (sharks, trip.code %in% sj.trip$Trip.code) # 3776 sharks
+sj.sharks <- filter (sharks, Trip.code %in% sj.trip$Trip.code) # 3776 sharks
 
 write.csv (sj.sharks, file = "../PDsharks/SanJose_sharks.csv", row.names = FALSE)
+
+#####
+# Also write csv of svry shark trips
+
+svry.shark.trips <- filter (gn.trips, Trip.code %in% svry.sharks$Trip.code)
+write.csv (svry.shark.trips, file = "../PDsharks/Svry_shark_trips.csv")
+
+
+### Also shark sets? These are sets that caught sharks, not all sets from trips targeting sharks. It might be worth also looking at trips/sets that targeted sharks but didn't catch them. 
+svry.shark.sets <- read_csv ("./Data_tables/cleaned_sets.csv") %>%
+  filter (Lance.code %in% svry.sharks$Lance.code)
